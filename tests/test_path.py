14dfba12253cf55abfaf5ff4e7a4daa2bcca2566
Ananya Srivastava <ananyasr@andrew.cmu.edu>
2022-04-29 13:08:51 +0000

    added tests for paths and functions

---
+++ tests/test_path.py
@@ -0,0 +1,12 @@
+import os 
+def test_data():
+    assert os.path.exists('./data')
+
+def test_license():
+    assert os.path.exists('./h2activity_package/LICENSE.md')
+
+def test_readme():
+    assert os.path.exists('./h2activity_package/README.md')
+
+def test_src():
+    assert os.path.exists('./h2activity_package/h2activity/src')
