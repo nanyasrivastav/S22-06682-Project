48aedcf548cbf1ce49a7be3e5ba9815e3ac530a2
Ananya Srivastava <ananyasr@andrew.cmu.edu>
2022-04-29 19:23:44 +0000

    pytest and make all work well now

--- src/tests/test_path.py
+++ src/tests/test_path.py
@@ -4,29 +4,29 @@ import os
 
 def test_data():
     """Passes if working directory has a data folder with datafiles."""
-    assert os.path.exists("../../data")
+    assert os.path.exists("../data")
 
 
 def test_src():
     """Passes if working directory contains src package."""
-    assert os.path.exists("../../src")
+    assert os.path.exists("../src")
 
 
 def test_license():
     """Passes if package contains LICENSE."""
-    assert os.path.exists("../../src/LICENSE")
+    assert os.path.exists("../src/LICENSE")
 
 
 def test_readme():
     """Passes if package contains README/ documentation."""
-    assert os.path.exists("../../src/README.md")
+    assert os.path.exists("../src/README.md")
 
 
 def test_setup():
     """Passes if package contains setup.py."""
-    assert os.path.exists("../../src/setup.py")
+    assert os.path.exists("../src/setup.py")
 
 
 def test_data_examples():
     """Passes if package contains example datafiles to work on."""
-    assert os.path.exists("../../src/h2activity/data_examples")
+    assert os.path.exists("../src/h2activity/data_examples")
