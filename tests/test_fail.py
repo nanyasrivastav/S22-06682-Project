d54e8b5b0f32b32a61ab0e095116ab1af5c0c54d
Ananya Srivastava <ananyasr@andrew.cmu.edu>
2022-04-29 15:55:03 +0000

    changed back to original imports in test fail

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -1,14 +1,14 @@
 import pytest
-#from h2activity_package.h2activity.src.utilities import display_head, max_h2, stats
+from h2activity_package.h2activity.src.utilities import display_head, max_h2, stats
 
 def test_fail_displayhead():
     with pytest.raises(TypeError):
-        h2activity_package.h2activity.src.utilities.display_head()
+        display_head()
 
 def test_fail_maxh2():
     with pytest.raises(TypeError):
-        h2activity_package.h2activity.src.utilities.max_h2()
+        max_h2()
 
 def test_fail_stats():
     with pytest.raises(TypeError):
-        h2activity_package.h2activity.src.utilities.stats()
+        stats()
